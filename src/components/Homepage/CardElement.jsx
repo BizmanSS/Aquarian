528b237d94ceac28f9b93624f797f4d5ef8d53ef
vivek <stephengmre@gmail.com>
2024-04-26 18:06:53 +0530

    merged09

--- src/components/Homepage/CardElement.jsx
+++ src/components/Homepage/CardElement.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const CardElement = ({ data }) => {
   return (
-    <div className='max-w-[21rem] rounded-2xl overflow-hidden border-[2px] border-[#939293] hover:border-transparent hover:bg-[#55FBEB] hover:shadow-lg mb-10 transition ease-in delay-100 duration-300'>
-      <img className='w-full p-2' src={data.img} alt='amenities' />
+    <div className='max-w-[21rem] h-[30rem] rounded-2xl overflow-hidden border-[2px] border-[#939293] hover:border-transparent hover:bg-[#55FBEB] hover:shadow-lg mb-10 transition ease-in delay-100 duration-300'>
+      <img className='w-[100%] h-[15rem] p-2' src={data.img} alt='amenities' />
       <div className='px-6 py-4'>
         <div className='font-semibold text-xl mb-2 text-[#009889] flex items-center justify-center'>
           {data.title}
--- src/components/Homepage/Component2.jsx
+++ src/components/Homepage/Component2.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import Card from './CardElement';
 import cardimg1 from '../../Assets/PR 1.jpg';
-import cardimg2 from '../../Assets/cardimg2.png';
-import cardimg3 from '../../Assets/cardimg3.png';
+import cardimg2 from '../../Assets/Family Sponsorship 1.jpg';
+import cardimg3 from '../../Assets/Super Visa 1.jpg';
+import cardimg4 from '../../Assets/Work Visa 3.jpg';
+import cardimg5 from '../../Assets/Studnet Visa 3.jpg';
+import cardimg6 from '../../Assets/Visa Extension 2.jpg';
+import cardimg7 from '../../Assets/Business Visa 2.jpg';
+import cardimg8 from '../../Assets/Visitor visa 2.jpg';
+import cardimg9 from '../../Assets/investors 2.jpg';
+
 const Component2 = () => {
   const array = [
     {
@@ -24,37 +31,37 @@ const Component2 = () => {
         'Enjoy hassle-free family visits! The Super Visa allows parents and grandparents of Canadian citizens and permanent residents to stay for up to two years per visit, with multiple entries.',
     },
     {
-      img: cardimg1,
+      img: cardimg4,
       title: 'Work Visa',
       description:
         'Explore infinite career opportunities in Canada! With a Work Visa, you can legally work, gain valuable experience, advance your career, and contribute to the countrys thriving workforce.',
     },
     {
-      img: cardimg2,
+      img: cardimg5,
       title: 'Student Visa',
       description:
         'Pursue your academic aspirations at prestigious Canadian institutions making you independent and ready for a successful career.',
     },
     {
-      img: cardimg3,
+      img: cardimg6,
       title: 'Visa Extensions',
       description:
         'Extend your stay and continue your Canadian adventure! Visa Extensions provide flexibility for individuals already in Canada, allowing them to prolong their stay for tourism, work, or study.',
     },
     {
-      img: cardimg1,
+      img: cardimg7,
       title: 'Business Visa',
       description:
         'Establish your name in Canadas dynamic business environment! A Business Visa opens doors for investors, entrepreneurs, and self-employed individuals to establish and grow their ventures in Canada.',
     },
     {
-      img: cardimg2,
+      img: cardimg8,
       title: 'Visitor Visa',
       description:
         'Explore Canadas beauty with a Visitor Visa! Temporary entry allows you to experience stunning landscapes, rich culture, and lasting memories.',
     },
     {
-      img: cardimg3,
+      img: cardimg9,
       title: 'Refusals',
       description:
         'Explore Canadas beauty with a Visitor Visa! Temporary entry allows you to experience stunning landscapes, rich culture, and lasting memories.',
