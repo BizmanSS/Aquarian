3b4fe144c997a64cb031feacd4ce549ad144de44
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-04-26 17:19:46 +0530

    Podcast Remove

--- src/components/globalComponents/Sidebar/Sidebar.jsx
+++ src/components/globalComponents/Sidebar/Sidebar.jsx
@@ -10,9 +10,6 @@ const Sidebar = () => {
         Resources & Tools
       </div>
       <div className="w-full h-[25%] text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300">
-        Podcasts
-      </div>
-      <div className="w-full h-[25%] text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300">
         Aquarian Services
       </div>
       <div className="w-full h-[25%] text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300">
