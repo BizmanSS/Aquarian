ab9f5ed9be06037727dcae1252f7a04e93cd5cc3
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-04-30 12:39:36 +0530

    Changes

--- src/components/AboutUs/Competitive.js
+++ src/components/AboutUs/Competitive.js
@@ -2,12 +2,41 @@ import React, { useState } from "react";
 import { IoMdAdd, IoMdRemove } from "react-icons/io";
 import "../../styles/AboutUs.css";
 import Expertise from "../../Assets/Expertise.png";
+import Integrity from "../../Assets/Integrity and trust.png";
+import One from "../../Assets/one to one solution.png";
+import Efficiency from "../../Assets/Efficiency and Accuracy.png";
+import Free from "../../Assets/free assessment.png";
 const Competitive = () => {
   const [expanded, setExpanded] = useState({});
-  const questions = ["EXPERTISE AND EXPERIENCE"];
+  const questions = [
+    "Expertise & Experience",
+    "Integrity and Trust",
+    "One-on-One Solutions",
+    "Efficiency and Accuracy",
+    "Free Online Assessment",
+  ];
+  const imageAdvantage = [Expertise, Integrity, One, Efficiency, Free];
 
   const answers = [
     "At Aquarian Immigration we pride ourselves on our extensive knowledge and expertise in Canadian immigration laws and regulations. Our team of immigration experts stays up to date with the latest policies and procedures to provide accurate and reliable advice to our clients. With staff having more that 25+ years of experience in the field, we have successfully assisted numerous clients in achieving their Canadian immigration goals.",
+    "At Aquarian Immigration, we prioritise the best interests of our clients. We believe in providing genuine and honest advice tailored to each individual's unique circumstances. Our team takes the time to understand our clients' goals, aspirations, and concerns, allowing us to provide personalised and strategic immigration solutions. Our commitment to integrity ensures that our clients receive honest assessments of their eligibility and realistic expectations regarding their immigration prospects.",
+    "We understand that the immigration process can be overwhelming and complex. That's why we believe in providing personalized attention and handholding to our clients throughout their immigration journey. Our dedicated team of professionals will be there for you every step of the way, offering guidance, support, and reassurance. We believe in building strong relationships with our clients based on trust, transparency, and open communication.",
+    "We understand that the immigration process can be overwhelming and complex. That's why we believe in providing personalized attention and handholding to our clients throughout their immigration journey. Our dedicated team of professionals will be there for you every step of the way, offering guidance, support, and reassurance. We believe in building strong relationships with our clients based on trust, transparency, and open communication.",
+    <div>
+      <p1>
+        <br />
+        We offer free online assessment tools and consultations making
+        immigration to Canada easily accessible to you.
+        <br />
+        <br />
+      </p1>
+      <div className="button">
+        <br />
+        FREE ASSESSMENT
+        <br />
+        <br />
+      </div>
+    </div>,
   ];
   const toggleExpand = (index) => {
     setExpanded((prevExpanded) => {
@@ -23,7 +52,10 @@ const Competitive = () => {
   return (
     <div className="Competitve">
       <h5 className="heading">Competitive advantages</h5>
-      <div style={{ marginLeft: "15%" }} className="line"></div>
+      <div
+        style={{ marginLeft: "15%", marginBottom: "3rem" }}
+        className="line"
+      ></div>
       {questions.map((item, index) => (
         <React.Fragment key={index}>
           <div
@@ -32,7 +64,7 @@ const Competitive = () => {
               display: "flex",
               justifyContent: "space-between",
               background: "#C9C9C9",
-              marginTop: "3rem",
+              marginTop: "0.1rem",
             }}
           >
             {" "}
@@ -45,8 +77,10 @@ const Competitive = () => {
               }}
             >
               <br />
-              {item}
-              <br />
+              <div className="gapImage">
+                <img alt="" src={imageAdvantage[index]} />
+                <h1>{item}</h1>
+              </div>
               <br />
             </div>
             <div
