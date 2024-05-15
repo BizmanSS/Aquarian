42e708f1cde49e8d5ae92d7519c76c88f702bf4e
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-05-15 18:00:54 +0530

    Done

--- src/components/globalComponents/Sidebar/Sidebar.jsx
+++ src/components/globalComponents/Sidebar/Sidebar.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import '../../../styles/Sidebar.css';
+import React from "react";
+import "../../../styles/Sidebar.css";
+import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-      <div className='w-full h-[25%] border-b border-gray-500 text-white text-center flex items-center justify-center hover:bg-[#01F9E1] bg-[#242525] hover:text-black cursor-pointer transition ease-in delay-100 duration-300'>
+    <div className="sidebar">
+      <div className="w-full h-[25%] border-b border-gray-500 text-white text-center flex items-center justify-center hover:bg-[#01F9E1] bg-[#242525] hover:text-black cursor-pointer transition ease-in delay-100 duration-300">
         Free Assessment
       </div>
-      <div className='w-full h-[25%] border-b border-gray-500 text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300'>
+      <div className="w-full h-[25%] border-b border-gray-500 text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300">
         Resources & Tools
       </div>
 
-      <div className='w-full h-[25%] border-b border-gray-500 text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300'>
+      <div className="w-full h-[25%] border-b border-gray-500 text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300">
         Aquarian Services
       </div>
-      <div className='w-full h-[25%] border-b border-gray-500 text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300'>
+      <Link
+        to="/blogs"
+        className="w-full h-[25%] border-b border-gray-500 text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300"
+      >
         Blog
-      </div>
-      <div className='w-full h-[25%] text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300'>
+      </Link>
+      <div className="w-full h-[25%] text-white text-center flex items-center justify-center hover:bg-[#01F9E1] hover:text-black cursor-pointer transition ease-in delay-100 duration-300">
         Newsroom
       </div>
     </div>
