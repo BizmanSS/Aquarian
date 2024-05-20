fe9005642aeabc74d201125958d9d9bf694bd5f6
Harsh Kumar Rattewal <harshrattewal@gmail.com>
2024-05-20 14:18:49 +0530

    Updated Sitemap

--- src/sitemapImmigrate.js
+++ src/sitemapImmigrate.js
@@ -1,41 +1,156 @@
 const sitemapImmigrate = [
   {
-    title: "Intro to PR",
+    title: "Provincial Nominee Program",
     linkTo: "",
+    nestedLinks: [
+      { title: "Alberta Immigrant Nominee Program", linkTo: "", 
+        nestedLinks:[
+          {title:"Alberta Opportunity Stream", linkTo:""}, 
+          {title:"Alberta Express Entry", linkTo:""},
+          {title:"Alberta Accelerated Tech Pathway", linkTo:""},
+          {title:"Farm Stream", linkTo:""},
+          {title:"Graduate Entrepreneur Stream", linkTo:""},
+          {title:"Foreign Graduate Entrepreneur Stream", linkTo:""},
+        ] 
+      },
+      { title: "British Columbia", linkTo: "",
+        nestedLinks:[
+          {title:"Skills Immigration Stream", linkTo:""}, 
+          {title:"BC PNP Tech", linkTo:""}, 
+          {title:"Entrepreneur Immigration Stream", linkTo:""}, 
+        ]
+       },
+      { title: "Manitoba", linkTo: "",
+        nestedLinks:[
+          {title:"Skilled Workers in Manitoba", linkTo:""},
+          {title:"Skilled Workers Overseas", linkTo:""},
+          {title:"International Education Stream", linkTo:""},
+          {title:"Business Investor Stream", linkTo:""},
+        ]
+       },
+      { title: "New Brunswick", linkTo: "",
+        nestedLinks:[
+          {title:"New Brunswick Express Entry Labor Market Stream", linkTo:""}, 
+          {title:"New Brunswick Strategic Initiative", linkTo:""},
+          {title:"New Brunswick Entrepreneurial Stream (closed)", linkTo:""},
+          {title:"Post-Graduate Entrepreneurial Stream", linkTo:""},
+          {title:"Skilled Worker Applicants with Employer Support", linkTo:""},
+          {title:"New Brunswick Business Immigration Stream", linkTo:""},
+          {title:"Atlantic Immigration Pilot Program", linkTo:""},
+        ] 
+       },
+      { title: "Newfoundland and Labrador", linkTo: "",
+        nestedLinks:[
+          {title:"Express Entry Skilled Worker Category", linkTo:""},
+          {title:"Skilled Worker Category", linkTo:""},
+          {title:"International Graduate Category", linkTo:""},
+          {title:"International Entrepreneur Category", linkTo:""},
+          {title:"International Graduate Entrepreneur Category", linkTo:""},  
+        ]
+       },
+      { title: "Northwest Territories", linkTo: "",
+        nestedLinks:[
+          {title:"Northwest Territories Express Entry", linkTo:""},
+          {title:"Northwest Territories Employer Driven", linkTo:""},
+          {title:"Critical Impact Worker", linkTo:""},
+          {title:"Northwest Territories Skilled Worker", linkTo:""},
+          {title:"Northwest Territories Business Driven", linkTo:""},
+          {title:"Northwest Territories Entrepreneur", linkTo:""},
+        ]
+       },
+      { title: "Nova Scotia", linkTo: "",
+        nestedLinks:[
+          {title:"Nova Scotia Experience: Express Entry", linkTo:""},
+          {title:"Entrepreneur Category", linkTo:""},
+          {title:"International Graduate Entrepreneur Category", linkTo:""},
+          {title:"International Graduates In Demand", linkTo:""},
+          {title:"Skilled Worker Category", linkTo:""},
+          {title:"Physician Stream", linkTo:""},
+          {title:"Nova Scotia Labor Market Priorities", linkTo:""},
+          {title:"Nova Scotia Labor Market Priorities for Physicians", linkTo:""},
+          {title:"Occupations In Demand", linkTo:""},
+          {title:"Critical Construction Worker Pilot", linkTo:""},
+        ]
+       },
+      { title: "Ontario", linkTo: "",
+        nestedLinks:[
+          {title:"Ontario Human Capital", linkTo:"",
+            nestedLinks:[
+              {title:"Ontario’s Express Entry Streams", linkTo:""},
+              {title:"Ontario Master’s Graduates", linkTo:""},
+              {title:"Ontario PhD Graduates", linkTo:""},
+            ]
+          },
+          {title:"Ontario Employer Job Offer", linkTo:""},
+          {title:"Ontario Entrepreneur", linkTo:""},
+          {title:"Regional Immigration Pilot", linkTo:""},
+        ]
+       },
+      { title: "Prince Edward Island", linkTo: "",
+        nestedLinks:[
+          {title:"PEI PNP Express Entry", linkTo:""},
+          {title:"Business Impact", linkTo:""},
+          {title:"Work Permit", linkTo:""},
+          {title:"Labor Impact", linkTo:""},
+          {title:"Skilled Worker in PEI", linkTo:""},
+          {title:"Skilled Worker Outside Canada", linkTo:""},
+          {title:"Critical Worker", linkTo:""},
+          {title:"International Graduate", linkTo:""},
+        ]
+       },
+      { title: "Saskatchewan", linkTo: "",
+        nestedLinks:[
+          {title:"International Skilled Worker", linkTo:""},
+          {title:"Saskatchewan Experience", linkTo:""},
+          {title:"Saskatchewan Entrepreneur and Farm", linkTo:""},
+        ]
+       },
+      { title: "Yukon", linkTo: "",
+        nestedLinsk:[
+          {title:"Yukon Express Entry", linkTo:""},
+          {title:"Skilled Worker Page", linkTo:""},
+          {title:"Critical Impact Worker", linkTo:""},
+          {title:"Business Nominee", linkTo:""},
+          {title:"Yukon Community Pilot", linkTo:""},
+        ]
+       },
+    ],
   },
   {
     title: "Express Entry",
     linkTo: "",
     nestedLinks: [
-      { title: "Intro", linkTo: "" },
       { title: "Federal Skilled Worker Class", linkTo: "" },
+      { title: "Canadian Experience Class (CEC)", linkTo: "" },
       { title: "Federal Skilled Trades Class", linkTo: "" },
-      { title: "Canadian Experience Class", linkTo: "" },
-      { title: "Comprehensive Ranking System (CRS)", linkTo: "" },
+      { title: "Comprehensive Ranking System (CRS) Score", linkTo: "",
+        nestedLinks:[
+          { title: "CRS Score Calculator", linkTo: "" },
+        ]
+       },
+      { title: "Express Entry Draws", linkTo: "" },
+      { title: "Invitation to Apply (ITA) & the electronic Application for Permanent Residence (eAPR)", linkTo: "" },
     ],
   },
-  {
-    title: "Provincial Nominee Program",
+  { title: "After Permanent Residency", 
     linkTo: "",
-    nestedLinks: [
-      { title: "PNP Intro", linkTo: "" },
-      { title: "Alberta", linkTo: "" },
-      { title: "British Columbia", linkTo: "" },
-      { title: "Manitoba", linkTo: "" },
-      { title: "New Brunswick", linkTo: "" },
-      { title: "Newfoundland and Labrador", linkTo: "" },
-      { title: "Northwest Territories", linkTo: "" },
-      { title: "Nova Scotia", linkTo: "" },
-      { title: "Ontario", linkTo: "" },
-      { title: "Prince Edward Island", linkTo: "" },
-      { title: "Saskatchewan", linkTo: "" },
-      { title: "Yukon", linkTo: "" },
-    ],
-  },
-  { title: "After PR", linkTo: "" },
-  { title: "PR FAQs", linkTo: "" },
-  { title: "Rural and Northern Immigration Pilot", linkTo: "" },
-  { title: "Agri-Food Pilot", linkTo: "" },
+    nestedLinks:[
+      {title:"Permanent Resident Cards", linkTo:""},
+      {title:"Permanent Resident Card Renewal", linkTo:""},
+      {title:"Permanent Resident Travel Document", linkTo:""},
+      {title:"Residency Obligations", linkTo:""},
+      {title:"Canadian Citizenship", linkTo:""},
+    ]
+   },
+  { title: "Agri Food Pilot", linkTo: "" },
+  { title: "Atlantic Immigration Program", linkTo: "" },
+  { title: "Business Class", 
+    linkTo: "",
+    nestedLinks:[
+      {title:"Federal Self-Employed", linkTo:""},
+      {title:"Canada Start-Up Visa (SUV)", linkTo:""},
+    ]
+   },
   {
     title: "Caregiver Programs",
     linkTo: "",
@@ -44,7 +159,9 @@ const sitemapImmigrate = [
       { title: "Home Support Worker Pilot", linkTo: "" },
     ],
   },
-  { title: "AIP Atlantic Immigration Program", linkTo: "" },
+  { title: "Canadian Immigration FAQs", linkTo: "" },
+  { title: "Rural and Northern Immigration Pilot", linkTo: "" },
+  { title: "TR to PR Route", linkTo: "" },
 ];
 
 export default sitemapImmigrate;
