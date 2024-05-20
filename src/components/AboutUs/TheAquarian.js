b5fffb0831816e7a18f02e45bd53fd2e0fb0a055
Harsh Kumar Rattewal <harshrattewal@gmail.com>
2024-05-20 18:30:27 +0530

    Color change

--- src/components/AboutUs/TheAquarian.js
+++ src/components/AboutUs/TheAquarian.js
@@ -51,6 +51,7 @@ const TheAquarian = () => {
             {!isActive && (
               <div
                 className="button"
+                style={{backgroundColor:"#3E3E41", color:"#EEEEEE"}}
                 onMouseEnter={() => !isMobile && setIsActive(true)}
                 onMouseLeave={() => !isMobile && setIsActive(false)}
                 onClick={handleClick}
@@ -61,6 +62,7 @@ const TheAquarian = () => {
             {isActive && (
               <div
                 className="button2"
+                style={{backgroundColor:"#3E3E41", color:"#EEEEEE"}}
                 onMouseLeave={() => !isMobile && setIsActive(false)}
                 onClick={handleClick}
               >
